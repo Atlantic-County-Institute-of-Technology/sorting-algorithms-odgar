80b5449f79a6455aae6149f444e849cb96eda080
oscar <159032472+odgar@users.noreply.github.com>
2026-01-07 10:06:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import random
-from bubblesort import bubble_sort
-from insertionsort import insertion_sort
-from selectionsort import selection_sort
+from bubble_sort import bubble_sort
+from insertion_sort import insertion_sort
+from selection_sort import selection_sort
 
 # original list of 10 random numbers
 
@@ -12,37 +12,53 @@ def clear():
     else:  #macOS
         _ = os.system('clear')
 
-def main_menu():
-
-    #data checking
-    def restart():
-        clear()
-        print("Invalid input, try again. \n" \
-        "Remember: \n" \
-        "- Ranges cannot be negative\n" \
-        "- Integer inputs only")
-        main_menu()
+def restart():
+    clear()
+    input("Invalid input, try again. \n" \
+    "Remember: \n" \
+    "- Ranges cannot be negative or greater than 100\n" \
+    "- Integer inputs only \n"
+    "\n Click any key to continue..."
+    )
+    clear()
 
     def original():
-        print("\nUnsorted: " , nums)
+        print("\nUnsorted: " , nums)    
 
-    def ask():
-        try:
-            print("\n Range? \n")
-            range1 = int(input("From: "))
-            if range1 >= 0:
-                try:  
-                    range2 = int(input("To:"))
-                    if range2 <= range1 or ((range2-range1 > 100)):
-                        restart()
-                except ValueError:
-                    restart()
-            global nums
-            nums = [random.randint(1,100) for x in range(range1,range2)]
-            original()
-        except ValueError:
-            restart()
+def ask():
+    range1 = int(input("\n Range? \n""From:"))
+    if range1 <= 0: 
+        restart()
+    range2 = int(input("To: "))
+    if range2 <= 0: restart
+    if range2 <= range1: restart
+    if range2-range1 > 100: restart()
 
+    try:
+        nums = [random.randint(1,100) for x in range(range1,range2)]
+    except Exception:
+        nums = "Error"
+    return nums
+
+    # def ask():
+    #     try:
+    #         print("\n Range? \n")
+    #         range1 = int(input("From: "))
+    #         if range1 >= 0:
+    #             try:  
+    #                 range2 = int(input("To:"))
+    #                 if range2 <= range1 or ((range2-range1 > 100)) or range >= 0:
+    #                     restart()
+    #             except ValueError:
+    #                 restart()
+    #         nums = [random.randint(1,100) for x in range(range1,range2)]
+    #         original()
+    #     except ValueError:
+    #         restart()
+    #     return nums
+
+
+def main_menu():
     try:
         option = int(input("------------------------------\n"
                         "| [*] Choose Your Algorithm. |\n"
@@ -53,14 +69,20 @@ def main_menu():
                         "------------------------------\n"
                         "\n [*] Your Choice: "))
         if option == 1:
-            ask()
-            bubble_sort(nums)
+            try: 
+                clear()
+                print("[Bubble Sort]")
+                bubble_sort(nums=ask())
+            #accounts for non-numerical exceptions
+            except Exception: restart()
         elif option == 2:
-            ask()
-            insertion_sort(nums)
+            clear()
+            print("[Insertion Sort]")
+            insertion_sort(nums=ask())
         elif option == 3:
-            ask()
-            selection_sort(nums)
+            clear()
+            print("[Selection Sort]")
+            selection_sort(nums=ask())
         elif option == 4:
             print("Exiting Program.")
             exit()
@@ -70,4 +92,5 @@ def main_menu():
         restart()
 
 
-main_menu()
+while True:
+    main_menu()
